66f38947ef3d4ecffc5c529d00ebd4e565ed2139
Hiroyoshi Kurohara <kurohara@yk.rim.or.jp>
2022-05-15 16:20:36 +0900

    under initial design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/App.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <div id="root"></div>
-    <!-- Load React. -->
-    <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
-    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-
-    <!-- Load our React component. -->
-    <script src="{{ asset('js/index.js') }}"></script>
+    <div class="container">
+        <header>
+            <!-- -->
+            <div class="sm:w-24 w-12"><x-fluentui-mail-inbox-all-24 /></div>
+            <label for="hamburger">
+                <x-radix-hamburger-menu class="sm:invisible w-8" />
+            </label>
+            <div>
+                <input type="checkbox" name="" id="hamburger" class="peer hidden"/>
+                <ul class="invisible peer-checked:visible sm:visible border-2 rounded-b">
+                    <li>About</li>
+                    <li>Register</li>
+                    <li>Logout</li>
+                    <li>Login</li>
+                    <li>Settings</li>
+                    <li>MailBox</li>
+                </ul>
+            </div>
+            <!-- -->
+            
+        </header>
+    </div>
 </body>
 </html>
